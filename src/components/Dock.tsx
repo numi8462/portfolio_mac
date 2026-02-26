7f8d2f78f31c0fb092ee348bc0db6b4372172fb8
Youngho Kim <yhk8462@naver.com>
2026-02-26 16:46:11 +0900

    feat: dock 기능 추가

---
+++ src/components/Dock.tsx
@@ -0,0 +1,106 @@
+import { DOCK_APPS } from '@constants/navbar';
+import { useGSAP } from '@gsap/react';
+import { useRef } from 'react';
+import { Tooltip } from 'react-tooltip';
+import gsap from 'gsap';
+
+type DockApp = (typeof DOCK_APPS)[number];
+
+const Dock = () => {
+  const dockRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    const dock = dockRef.current;
+
+    if (!dock) return;
+
+    const icons = dock.querySelectorAll('.dock-icon');
+
+    const animateIcons = (mouseX: number) => {
+      const { left } = dock.getBoundingClientRect();
+
+      icons.forEach((icon) => {
+        const { left: iconLeft, width } = icon.getBoundingClientRect();
+        const center = iconLeft - left + width / 2;
+        const distance = Math.abs(mouseX - center);
+
+        const intensity = Math.exp(-(distance ** 2.5) / 5000);
+
+        gsap.to(icon, {
+          scale: 1 + 0.25 * intensity,
+          y: -15 * intensity,
+          duration: 0.2,
+          ease: 'power1.out',
+        });
+      });
+    };
+
+    const handleMouseMove = (e) => {
+      const { left } = dock.getBoundingClientRect();
+
+      animateIcons(e.clientX - left);
+    };
+
+    const resetIcons = () => {
+      icons.forEach((icon) => {
+        gsap.to(icon, {
+          scale: 1,
+          y: 0,
+          duration: 0.3,
+          ease: 'power1.out',
+        });
+      });
+    };
+
+    dock.addEventListener('mousemove', handleMouseMove);
+    dock.addEventListener('mouseleave', resetIcons);
+
+    return () => {
+      dock.removeEventListener('mousemove', handleMouseMove);
+      dock.removeEventListener('mouseleave', resetIcons);
+    };
+  }, []);
+
+  const toogleApp = (app: DockApp) => {
+    //todo: 아이콘 토글 추가
+  };
+
+  return (
+    <section className="absolute bottom-5 left-1/2 -translate-x-1/2 z-50 select-none max-sm:hidden">
+      <div
+        ref={dockRef}
+        className="bg-white/20 backdrop-blur-md justify-between rounded-2xl p-1.5 flex items-end gap-1.5"
+      >
+        {DOCK_APPS.map((app) => (
+          <div key={app.id} className="relative flex justify-center">
+            <button
+              type="button"
+              className="size-14 3xl:size-20 cursor-pointer"
+              aria-label={app.name}
+              data-tooltip-id="dock-tooltip"
+              data-tooltip-content={app.name}
+              data-tooltip-delay-show={150}
+              disabled={!app.canOpen}
+              onClick={() => toogleApp(app)}
+            >
+              <img
+                src={`/images/${app.icon}`}
+                alt={app.name}
+                loading="lazy"
+                className={`dock-icon object-cover object-center ${app.canOpen ? '' : 'opacity-60'}`}
+              />
+            </button>
+          </div>
+        ))}
+
+        <Tooltip
+          id="dock-tooltip"
+          place="top"
+          className="py-1! px-3! w-fit! text-center! text-xs! rounded-md! bg-blue-200! text-blue-900! shadow-2xl!"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Dock;
