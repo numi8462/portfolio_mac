1b4fe86f6ae03b81a061ad045e014a031c0c4815
Youngho Kim <yhk8462@naver.com>
2026-02-26 17:52:09 +0900

    feat: dock.ts 추가

---
+++ src/constants/dock.ts
@@ -0,0 +1,38 @@
+export const DOCK_APPS = [
+  {
+    id: 'finder',
+    name: '포트폴리오',
+    icon: 'finder.png',
+    canOpen: true,
+  },
+  {
+    id: 'safari',
+    name: 'Articles',
+    icon: 'safari.png',
+    canOpen: true,
+  },
+  {
+    id: 'photos',
+    name: '갤러리',
+    icon: 'photos.png',
+    canOpen: true,
+  },
+  {
+    id: 'contact',
+    name: '연락',
+    icon: 'contact.png',
+    canOpen: true,
+  },
+  {
+    id: 'terminal',
+    name: '스킬',
+    icon: 'terminal.png',
+    canOpen: true,
+  },
+  {
+    id: 'trash',
+    name: '쓰레기통',
+    icon: 'trash.png',
+    canOpen: false,
+  },
+];
